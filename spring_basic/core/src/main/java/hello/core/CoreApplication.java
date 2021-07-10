bc1cf51db02035ebf6ef47b5b40c0a84a811cac3
woonsik <dnstlr2933@naver.com>
2021-07-10 18:18:40 +0900

    MyLogger와 ObjectProvider를 이용해서 Http요청 시 생성되는 Scope(request) 테스트

--- spring_basic/core/src/main/java/hello/core/CoreApplication.java
+++ spring_basic/core/src/main/java/hello/core/CoreApplication.java
@@ -1,7 +1,10 @@
 package hello.core;
 
+import hello.core.web.LogDemoController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class CoreApplication {
