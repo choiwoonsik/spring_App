1b56a2a4d122c33475f99a67173113ba5f142e48
woonsik <dnstlr2933@naver.com>
2021-07-16 17:48:36 +0900

    상품 검색 쿼리 작성 - 1) 문자열 jpql, 2) Criteria (두가지 방법 다 현업에서 사용하지 않는다. QueryDSL을 사용) 추후 QueryDSL로 변경

--- springBoot_JPA_1/jpaShop/src/main/java/jpaBook/jpaShop/repository/OrderRepository.java
+++ springBoot_JPA_1/jpaShop/src/main/java/jpaBook/jpaShop/repository/OrderRepository.java
@@ -1,10 +1,16 @@
 package jpaBook.jpaShop.repository;
 
+import jpaBook.jpaShop.domain.Member;
 import jpaBook.jpaShop.domain.Order;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.support.Querydsl;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -22,7 +28,53 @@ public class OrderRepository {
 	}
 
 	/*
-	검색 기능을 통해 조회
+	검색 기능을 통해 조회 (1) - String
 	 */
-//	public List<Order> findAll(OrderSearch orderSearch) {}
+	public List<Order> findAllByString(OrderSearch orderSearch) {
+
+		// 해당 jpql은 상태와 이름이 둘다 있는 경우에만 가능하다 -> 만약 하나씩만 있는 경우에는? (동적 쿼리필요)
+		return em.createQuery("select o from Order o join o.member m" +
+				"where o.status = :status" +
+				"and m.name like :name", Order.class)
+				.setParameter("status", orderSearch.getOrderStatus())
+				.setParameter("name", orderSearch.getMemberName())
+				.setMaxResults(1000) // 최대 1000건
+				.getResultList();
+
+
+	}
+
+	/*
+	검색 기능을 통해 조회 (2) - Criteria
+	 */
+	public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+		Root<Order> o = cq.from(Order.class);
+		Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
+		List<Predicate> criteria = new ArrayList<>();
+		//주문 상태 검색
+		if (orderSearch.getOrderStatus() != null) {
+			Predicate status = cb.equal(o.get("status"),
+					orderSearch.getOrderStatus());
+			criteria.add(status);
+		}
+		//회원 이름 검색
+		if (StringUtils.hasText(orderSearch.getMemberName())) {
+			Predicate name =
+					cb.like(m.<String>get("name"), "%" +
+							orderSearch.getMemberName() + "%");
+			criteria.add(name);
+		}
+		cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+		TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대1000건
+		return query.getResultList();
+	}
+
+	/*
+	검색 기능을 통해 조회 (3) - QueryDSL
+	 */
+//	public List<Order> findAll(OrderSearch orderSearch) {
+//
+//	}
 }
