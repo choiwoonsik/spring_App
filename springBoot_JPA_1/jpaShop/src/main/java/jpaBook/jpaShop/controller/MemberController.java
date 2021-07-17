ee4335d7f6a58e160ad1c9dc9aa3f4454fb347a4
woonsik <dnstlr2933@naver.com>
2021-07-18 00:12:31 +0900

    우편번호도 필수값으로 변경, 회원 전체 조회 구현 (thymeleaf 이용)

--- springBoot_JPA_1/jpaShop/src/main/java/jpaBook/jpaShop/controller/MemberController.java
+++ springBoot_JPA_1/jpaShop/src/main/java/jpaBook/jpaShop/controller/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -49,5 +50,12 @@ public class MemberController {
 		}
 
 		return "redirect:/";
+	}
+
+	@GetMapping("/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findAllMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
 	}
 }
--- springBoot_JPA_1/jpaShop/src/main/java/jpaBook/jpaShop/controller/MemberForm.java
+++ springBoot_JPA_1/jpaShop/src/main/java/jpaBook/jpaShop/controller/MemberForm.java
@@ -12,13 +12,14 @@ import javax.validation.constraints.Size;
 public class MemberForm {
 
 	@Size(max = 10, message = "10자 이상 입력할 수 없습니다.")
-	@NotEmpty(message = "회원 이름은 필수입니다.")
+	@NotEmpty(message = "성함을 입력해주세요.")
 	private String name;
 
 	@Email(message = "올바른 형식의 이메일을 입력하세요.")
-	@NotEmpty(message = "회원 e-mail은 필수입니다.")
+	@NotEmpty(message = "이메일을 입력해주세요.")
 	private String email;
 
+	@NotEmpty(message = "주소를 입력해주세요.")
 	private String zipcode;
 	private String streetAdr;
 	private String detailAdr;
