36769dc1ce59befcf8436dabc5ec2e76daa7612d
woonsik <dnstlr2933@naver.com>
2021-07-23 17:03:16 +0900

    주문 생성, 주문 목록 조회, 주문 취소 구현

---
+++ springBoot_JPA_1/jpaShop/src/main/java/jpaBook/jpaShop/controller/OrderController.java
@@ -0,0 +1,59 @@
+package jpaBook.jpaShop.controller;
+
+import jpaBook.jpaShop.domain.Member;
+import jpaBook.jpaShop.domain.Order;
+import jpaBook.jpaShop.domain.item.Item;
+import jpaBook.jpaShop.repository.OrderSearch;
+import jpaBook.jpaShop.service.ItemService;
+import jpaBook.jpaShop.service.MemberService;
+import jpaBook.jpaShop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class OrderController {
+
+	private final OrderService orderService;
+	private final MemberService memberService;
+	private final ItemService itemService;
+
+	@GetMapping("/order")
+	public String createForm(Model model) {
+		List<Member> members = memberService.findAllMembers();
+		List<Item> items = itemService.findAllItems();
+
+		model.addAttribute("members", members);
+		model.addAttribute("items", items);
+
+		return "order/createOrder";
+	}
+
+	@PostMapping("/order")
+	public String order(@RequestParam("memberId") Long memberId,
+						@RequestParam("itemId") Long itemId,
+						@RequestParam("count") int count) {
+		orderService.order(memberId, itemId, count);
+		return "redirect:/orders";
+	}
+
+	@GetMapping("/orders")
+	public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+		List<Order> orders = orderService.findAllByString(orderSearch);
+		model.addAttribute("orders", orders);
+
+		return "order/orderList";
+	}
+
+	@PostMapping("/orders/{orderId}/cancel")
+	public String cancelOrder(@PathVariable("orderId") Long orderId) {
+		orderService.cancelOrder(orderId);
+		return "redirect:/orders";
+	}
+}
--- springBoot_JPA_1/jpaShop/src/main/java/jpaBook/jpaShop/service/OrderService.java
+++ springBoot_JPA_1/jpaShop/src/main/java/jpaBook/jpaShop/service/OrderService.java
@@ -8,9 +8,15 @@ import jpaBook.jpaShop.domain.item.Item;
 import jpaBook.jpaShop.repository.ItemDaoImpl;
 import jpaBook.jpaShop.repository.MemberDaoImpl;
 import jpaBook.jpaShop.repository.OrderDaoImpl;
+import jpaBook.jpaShop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -20,6 +26,7 @@ public class OrderService {
 	private final OrderDaoImpl orderDaoImpl;
 	private final MemberDaoImpl memberDaoImpl;
 	private final ItemDaoImpl itemDaoImpl;
+	private final EntityManager em;
 
 	/**
 	 * 주문
@@ -54,6 +61,41 @@ public class OrderService {
 		Order order = orderDaoImpl.findOne(orderId);
 		// 주문 취소
 		order.cancel();
+	}
+
+	public List<Order> findAllByString(OrderSearch orderSearch) {
+		//language=JPAQL
+		String jpql = "select o From Order o join o.member m";
+		boolean isFirstCondition = true;
+		//주문 상태 검색
+		if (orderSearch.getOrderStatus() != null) {
+			if (isFirstCondition) {
+				jpql += " where";
+				isFirstCondition = false;
+			} else {
+				jpql += " and";
+			}
+			jpql += " o.status = :status";
+		}
+		//회원 이름 검색
+		if (StringUtils.hasText(orderSearch.getMemberName())) {
+			if (isFirstCondition) {
+				jpql += " where";
+				isFirstCondition = false;
+			} else {
+				jpql += " and";
+			}
+			jpql += " m.name like :name";
+		}
+		TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+				.setMaxResults(1000); //최대 1000건
+		if (orderSearch.getOrderStatus() != null) {
+			query = query.setParameter("status", orderSearch.getOrderStatus());
+		}
+		if (StringUtils.hasText(orderSearch.getMemberName())) {
+			query = query.setParameter("name", orderSearch.getMemberName());
+		}
+		return query.getResultList();
 	}
 
 	/**
